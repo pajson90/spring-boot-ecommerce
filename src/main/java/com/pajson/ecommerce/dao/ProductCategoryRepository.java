5f67122587dc2e63f186756f34a769810742cbfe
pajson90 <pavlebursac@gmail.com>
2020-12-09 00:10:43 +0100

    Add custom repo rest definition

--- src/main/java/com/pajson/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/pajson/ecommerce/dao/ProductCategoryRepository.java
@@ -2,7 +2,8 @@ package com.pajson.ecommerce.dao;
 
 import com.pajson.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
